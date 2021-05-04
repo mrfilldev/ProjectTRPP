5de7c55225513eb8e4b655ba45af4dbd90de94db
Vladislav Filippov <mr.filldev@gmail.com>
2021-05-05 00:18:14 +0300

    добавление Django приложения для отображения главной странички

--- anfisa4friends/settings.py
+++ anfisa4friends/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage'
+    # Здесь укажите имя приложения, которое надо подключить
 ]
 
 MIDDLEWARE = [
--- anfisa4friends/urls.py
+++ anfisa4friends/urls.py
@@ -1,5 +1,6 @@
-# anfisa4friends URL Configuration
 from django.urls import path
+from homepage import views as home_views
 
 urlpatterns = [
+    path('', home_views.index),
 ]
