f816c297c66d680fd6a3a263c3c82b829fb4dd7e
Данила <den-vedeshkin@rambler.ru>
2021-05-05 17:04:23 +0300

    Дебагинг. Устранение ошибок #2.

--- homepage/views.py
+++ homepage/views.py
@@ -2,28 +2,52 @@ from django.shortcuts import render
 from icecream.models import icecream_db
 from anfisa.models import friends_db
 from anfisa.services import what_weather
-# Импорт должен быть тут
 
 
 def index(request):
     icecreams = ''
     friends = ''
+    city_weather = ''
+    friend_output = ''
+    # Добавлена пустая  переменная для хранения сорта мороженого, полученного из запроса
+    selected_icecream = ''
 
     for friend in friends_db:
-        # Узнайте город друга и сохраните его в city
-        city = friends_db[friend]
-        # Получите погоду из функции what_weather() и сохраните её в переменную weather
-        weather = what_weather(city)
-        # Здесь через f-строку объедините переменные в одну строчку:
-        # имя :: город :: погода
-        friends += f'{friend} :: {city} :: {weather} <br>'
+        # Около каждого имени вставляется radio button,
+        # и теперь в форме кликом по кнопочке можно будет выбрать одного из друзей.
+        friends += (f'<input type="radio" name="friend"'
+                   f' required value="{friend}">{friend}<br>')
 
     for i in range(len(icecream_db)):
-        icecreams += (f'{icecream_db[i]["name"]} | '
-                    f'<a href="icecream/{i}/">Узнать состав</a><br>')
+        # В переменную ice_form добавьте HTML-код радио-кнопки и название мороженого 
+        # (за образец можно взять код для списка друзей)
+        ice_form = (f'<input type="radio" name="icecream" required ' 
+                    f'value="{icecream_db[i]["name"]}"> {icecream_db[i]["name"]}<br>')
+        
+        ice_link = f'<a href="icecream/{i}/">Узнать состав</a>'
+        icecreams += f'{ice_form} | {ice_link} <br>'
+
+    if request.method == 'POST':
+        # Извлекли из запроса имя друга
+        selected_friend = request.POST['friend']
+        # Сохраните  в переменную selected_icecream название мороженого, полученное в POST-запросе
+        selected_icecream = request.POST['icecream']
+
+        # В переменную city записано название города
+        city = friends_db[selected_friend]
+        # Запрос погоды в городе city
+        weather = what_weather(city)
+
+        # Вместо слова "мороженое" выведите название сорта из запроса.
+        friend_output = f'{selected_friend}, тебе прислали {selected_icecream}!'
+        city_weather = f'В городе {city} погода: {weather}'
 
     context = {
         'icecreams': icecreams,
         'friends': friends,
+        'friend_output': friend_output,
+        'city_weather': city_weather,
+        'selected_icecream': selected_icecream,
+
     }
     return render(request, 'homepage/index.html', context)
